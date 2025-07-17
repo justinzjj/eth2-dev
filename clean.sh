33c41023816c580e4ba3ce7e5a7108159972243e
justinzjj <zoujiajie2001@163.com>
2025-07-17 12:45:38 +0000

    添加 多节点 部署脚本 但是现在问题在于  node 2 和geth 2 无法同步区块头 node1 和node2 通过p2p已经可以链接 geth1 和geth2 暂未链接

--- clean.sh
+++ clean.sh
@@ -4,8 +4,11 @@
  # @filename: 
  # @version: 
  # @Description: 
- # @LastEditTime: 2025-07-17 05:09:38
+ # @LastEditTime: 2025-07-17 11:43:08
 ### 
 docker rm -f $(docker ps -a -q)
-rm -Rf ./consensus/beacondata ./consensus/validatordata ./consensus/genesis.ssz
-rm -Rf ./execution/geth
+rm -Rf ./consensus/node1/beacondata  ./consensus/genesis.ssz
+rm -Rf ./consensus/node2/beacondata  ./consensus/genesis.ssz
+rm -Rf ./consensus/validatordata
+rm -Rf ./execution/node1/geth
+rm -Rf ./execution/node2/geth
